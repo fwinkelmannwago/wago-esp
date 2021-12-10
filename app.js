47b3aeca6eef7d31e21f3f6e74e4c5cf30d64bb6
fwinkelmannwago <fabian.winkelmann@wago.com>
2021-12-10 09:55:03 +0100

    CHANGE CONTENT_SECURITY_POLICY HEADER

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.use(helmet())
 
 app.use(express.static(path.join(__dirname, "dist")))
 
+app.use((req,res) => {
+    res.setHeader("Content-Security-Policy", "script-src:'unsafe-inline'")
+})
+
 
 app.get("/api/ip/:name", async (req,res) => {
     const name = req.params.name;
