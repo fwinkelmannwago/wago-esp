5cf3e389a6fda2366b46e06c9f866863459f7312
fwinkelmannwago <fabian.winkelmann@wago.com>
2021-12-10 14:18:46 +0100

    UPDATE ALL FILES

--- appcopy.js
+++ appcopy.js
@@ -1,15 +1,35 @@
 const express = require("express");
-const path = require("path");
+const path = require("path")
+const rateLimit = require("express-rate-limit")
+const dotenv = require("dotenv");
 const app = express();
-const cors = require("cors");
+const xss = require("xss-clean")
+const helmet = require("helmet")
 const {addIp,getIp } = require("./datenbank.js")
-const dotenv = require("dotenv")
+const cors = require("cors");
+dotenv.config()
 
-app.use(cors())
-app.use(express.json());
+app.use(cors());
+app.use(express.json({limit: '10kb'}))
 
-app.use(express.static(path.join(__dirname, "dist", "client")))
+const limit = rateLimit({
+    max: 100,// max requests
+    windowMs: 60 * 60 * 1000, // 1 Hour
+    message: 'Too many requests' // message to send
+});
 
+app.use("/api/ip", limit);
+
+app.use(xss())
+
+app.use(helmet())
+
+app.use(express.static(path.join(__dirname, "dist")))
+
+app.use((req,res,next) => {
+    console.log(req.originalUrl);
+    next();
+})
 
 app.get("/api/ip/:name", async (req,res) => {
     const name = req.params.name;
@@ -52,7 +72,6 @@ app.get("/api/ip/:name", async (req,res) => {
     })
 })
 
-
 app.post("/api/ip", async (req,res) => {
     const {name, IP} = req.body;
     if (!name || !IP) {
@@ -82,18 +101,23 @@ app.post("/api/ip", async (req,res) => {
 
    
 })
-
-
+/*
+app.get("*", (req,res) => {
+    console.log(req.originalUrl)
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+})
+*/
 
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "dist", "client", "index.html"))
+    console.log("test");
+    res.send("test")
 })
 
+const PORT = process.env.PORT ?? 3000;
 
-
-const port = 9000;
-app.listen(port, () => {
-    console.log("app running on port " + port)
+app.listen(PORT, () => {
+    console.log(`APP LISTENING ON ${PORT}`)
 })
+
 
 
